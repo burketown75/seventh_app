19f9991a2157cb96bdfb258bedf361ccf5d6572a
Patrick E. Burke <uconnfootball75@yahoo.com>
2012-12-01 01:46:27 -0500

    chap10

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,20 @@
-class StaticPagesController < ApplicationController
-  def home
+class SessionsController < ApplicationController
+  def new
   end
 
-  def help
+  def create
+  	user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_back_or user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
-  def about
-  end
-
-  def contact
+  def destroy
+    sign_out
+    redirect_to root_url
   end
 end
